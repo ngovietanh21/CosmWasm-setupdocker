edf584d87d867b18a5d89db3bd436c2888a4fa62
ngovietanh21 <ngovietanh21@gmail.com>
2023-01-09 14:09:30 +0800

    first commit

---
+++ setup-docker.sh
@@ -0,0 +1,34 @@
+apt-get -y update
+apt-get -y install git
+apt-get -y install wget
+apt-get -y install curl
+apt-get -y install nano
+apt-get -y install jq
+
+# install make
+apt-get -y install build-essential  # for MAKE
+
+#install GO
+wget https://dl.google.com/go/go1.17.2.linux-amd64.tar.gz
+tar -xvf go1.17.2.linux-amd64.tar.gz
+mv go /usr/local
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo 'export GOROOT=/usr/local/go' >>~/.bash_profile
+echo 'export GOPATH=$HOME/go' >>~/.bash_profile
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >>~/.bash_profile
+source ~/.bash_profile
+echo 'source ~/.bash_profile' >> /etc/bash.bashrc
+
+
+rm go1.17.2.linux-amd64.tar.gz
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+echo "source $HOME/.cargo/env" >> ~/.bash_profile
+
+rustup default stable
+rustup update stable
+rustup target list --installed
+rustup target add wasm32-unknown-unknown
